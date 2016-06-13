aa2b2a25aac12338e26d6cded07c0e323bd32a05
ZaL <z4ls@live.com>
2016-06-13 14:29:52 +0300

    add payment webview

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -24,41 +24,44 @@ class PaymentController extends Controller
     {
         $weddingDate = $request->wedding_date;
         $amount = $request->amount;
+        $email = $request->email;
         $params = [
 
-//            ['name'=>'merchant','contents'=>'EPG2014'],
-//            ['name'=>'transaction_id','contents'=>uniqid()],
-//            ['name'=>'amount','contents'=>$request->get('amount')],
-//            ['name'=>'processpage','contents'=>url('api/v1/payment/process')],
-//            ['name'=>'sec_key','contents'=>'8h12dwrtu83d153'],
-//            ['name'=>'op_post','contents'=> 'false'],
-//            ['name'=>'md_flds','contents'=>'transaction_id:amount:processpage'],
-//            ['name'=>'user_mail','contents'=>$request->email],
-//            ['name'=>'currency','contents'=>'KWD'],
-//            ['name'=>'remotepassword','contents'=>'F82D2878'],
+            ['name'=>'merchant','contents'=>'EPG2014'],
+            ['name'=>'transaction_id','contents'=>uniqid()],
+            ['name'=>'amount','contents'=>$amount],
+            ['name'=>'processpage','contents'=>url('api/v1/payment/process')],
+            ['name'=>'sec_key','contents'=>'8h12dwrtu83d153'],
+            ['name'=>'op_post','contents'=> 'false'],
+            ['name'=>'md_flds','contents'=>'transaction_id:amount:processpage'],
+            ['name'=>'user_mail','contents'=>$email],
+            ['name'=>'currency','contents'=>'KWD'],
+            ['name'=>'remotepassword','contents'=>'F82D2878'],
 
-            'merchant'=>'EPG2014',
-            'transaction_id'=>uniqid(),
-            'amount'=>$request->get('amount'),
-            'processpage'=>url('api/v1/payment/process'),
-            'sec_key'=>'8h12dwrtu83d153',
-            'op_post'=> 'false',
-            'md_flds'=>'transaction_id:amount:processpage',
-            'user_mail'=>$request->email,
-            'currency'=>'KWD',
-            'remotepassword'=>'F82D2878',
+
         ];
 
-//        $client = new \GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client();
 //        $response = $client->request('POST', 'https://dealer.e.net.kw/merchant/payment', [
-//        $response = $client->request('POST', 'http://test.e.net.kw/Merchant/Payment/eNetCpgMainAPI.aspx', [
-//            'multipart' => $params
-//        ]);
+        $response = $client->request('POST', 'http://test.e.net.kw/Merchant/Payment/eNetCpgMainAPI.aspx', [
+            'multipart' => $params
+        ]);
 
-//        return $response;
+//            'merchant'=>'EPG2014',
+//            'transaction_id'=>uniqid(),
+//            'amount'=>$request->get('amount'),
+//            'processpage'=>url('api/v1/payment/process'),
+//            'sec_key'=>'8h12dwrtu83d153',
+//            'op_post'=> 'false',
+//            'md_flds'=>'transaction_id:amount:processpage',
+//            'user_mail'=>$request->email,
+//            'currency'=>'KWD',
+//            'remotepassword'=>'F82D2878',
+
+        return $response;
 
 
-        return view('module.payment.index',compact('params','amount'));
+//        return view('module.payment.index',compact('params','amount'));
     }
 
     public function paymentProcess(Request $request)
