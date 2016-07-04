d8d8af467f855b12ffb29d5d90d5e42dee057976
ZaL <z4ls@live.com>
2016-07-04 12:09:43 +0300

    add alert box

--- resources/views/module/payment/failure.blade.php
+++ resources/views/module/payment/failure.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
         <div class="center-block">
             <h1 style="text-align: center; padding-top: 100px; color:red">Transaction Failed</h1>
-            <div>
-                {{ $request }}
+            <div class="text-center">
+                <a href="{{route('payment.end')}}" class="btn btn-warning" role="button" style="text-align: center"><h2> Return to HomePage  </h2></a>
             </div>
         </div>
     </div>
--- resources/views/module/payment/success.blade.php
+++ resources/views/module/payment/success.blade.php
@@ -3,8 +3,8 @@
     <div class="container">
         <div class="center-block">
             <h1 style="text-align: center; padding-top: 100px; color:green">Transaction Success</h1>
-            <div>
-                <a href="{{route('payment.end')}}" class="btn btn-positive"><h2>  Return to HomePage  </h2></a>
+            <div class="text-center">
+                <a href="{{route('payment.end')}}" class="btn btn-positive" role="button" style="text-align: center"><h2>  Return to HomePage  </h2></a>
             </div>
         </div>
     </div>
