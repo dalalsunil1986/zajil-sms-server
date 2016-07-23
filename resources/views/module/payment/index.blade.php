53abcc37ab0a9c70c7e53af5c7c4e767da6b42ac
ZaL <z4ls@live.com>
2016-07-23 06:52:50 +0530

    paymet link

--- resources/views/module/payment/index.blade.php
+++ resources/views/module/payment/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            {!! Form::open(['url' => ['http://test.e.net.kw/Merchant/Payment/eNetCpgMainAPI.aspx'], 'method' => 'post'], ['class'=>'']) !!}
-{{--            {!! Form::open(['url' => ['https://dealer.e.net.kw/merchant/payment'], 'method' => 'GET'], ['class'=>'']) !!}--}}
+{{--            {!! Form::open(['url' => ['http://test.e.net.kw/Merchant/Payment/eNetCpgMainAPI.aspx'], 'method' => 'post'], ['class'=>'']) !!}--}}
+            {!! Form::open(['url' => ['https://dealer.e.net.kw/merchant/payment'], 'method' => 'GET'], ['class'=>'']) !!}
             {{--@foreach($params as $key )--}}
                 {{--{!! Form::hidden($key['name'],$key['contents']) !!}--}}
             {{--@endforeach--}}
