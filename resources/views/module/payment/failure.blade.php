ddffa2f08fee301c9e9428e4e24bc7691cf9eca1
ZaL <z4ls@live.com>
2016-09-03 12:26:45 +0530

    add soft delete to orders

--- resources/views/module/payment/failure.blade.php
+++ resources/views/module/payment/failure.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="center-block">
-            <h1 style="text-align: center; padding-top: 100px; color:red">Transaction Failed. Try Again. {{ $request }}</h1>
+            <h1 style="text-align: center; padding-top: 100px; color:red">Transaction Failed. Try Again.</h1>
         </div>
 
     </div>
