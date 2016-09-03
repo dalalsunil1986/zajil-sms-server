e0c5898478fd354c4da345a560e88549a6802efd
ZaL <z4ls@live.com>
2016-09-03 12:37:38 +0530

    add soft delete to orders

--- resources/views/module/payment/success.blade.php
+++ resources/views/module/payment/success.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="center-block">
-            <h1 style="text-align: center; padding-top: 100px; color:green">Transaction Success, {{ $request }}</h1>
+            <h1 style="text-align: center; padding-top: 100px; color:green">Transaction Success</h1>
             {{--<div class="text-center">--}}
                 {{--<a href="{{route('payment.success')}}" class="btn btn-success" role="button" style="text-align: center">--}}
                     {{--<h2>Return to Home</h2>--}}
